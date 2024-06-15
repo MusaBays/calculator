44a44a06b094f96420646bf8080e55d3ecf5d696
MusaBays <musabays123@mail.ru>
2024-06-16 02:16:41 +0300

    Update Calc.java

--- src/Cal/Calc.java
+++ src/Cal/Calc.java
@@ -18,13 +18,16 @@ public class Calc {
         System.out.print("Введите выражение: ");
         String vvod = s.nextLine();
         String[] spl = vvod.split(" ");
-        exceptions(spl);
-        arabicMath(spl);
-        convertRoman(spl);
+        calc(spl);
 
 
     }
 
+    public static void calc(String[] spl){
+        exceptions(spl);
+        arabicMath(spl);
+        convertRoman(spl);
+    }
     public static void exceptions(String[] spl) {
         if (spl.length != 3) {
             throw new IllegalArgumentException("Введено некорректное выражение.");
@@ -37,6 +40,8 @@ public class Calc {
         if ( (!roman.contains(spl[2])) && !arabic.contains(spl[2])) {
             throw new IllegalArgumentException("Введены некорректные данные");
         }
+        if ( (roman.contains(spl[0])) && arabic.contains(spl[2]) || (roman.contains(spl[2])) && arabic.contains(spl[0])) {
+            throw new IllegalArgumentException("Введены некорректные данные");}
 
     }
 
@@ -60,40 +65,36 @@ public class Calc {
     }
 
     private static void convertRoman(String[] spl) {
-        if (spl[0].equals("X")) {
-            spl[0] = "10";
-        }
-        if (spl[2].equals("X")) {
-            spl[2] = "10";
-        }
-        for (int i = 0; i < roman.size(); i++) {
-            if (spl[0].equals(roman.get(i))) {
-                spl[0] = arabic.get(i);
-            }
-
-            if (spl[2].equals(roman.get(i))) {
-                spl[2] = arabic.get(i);
-
-
-                int spl0 = Integer.parseInt(spl[0]);
-                int spl2 = Integer.parseInt(spl[2]);
-                String math = spl[1];
-                int calculation = Result(spl0, spl2, math);
-
-
-                if (calculation == 100) {
-                    System.out.println("С");
-                } else if (calculation % 10 == 0) {
-                    int s = calculation / 10;
-                    System.out.print(romanTens.get(s - 1));
-                } else if (((calculation % 100) / 10) != 0) {
-                    int s = (calculation % 100) / 10;
-                    System.out.print(romanTens.get(s - 1));
+        for (int j = 0; j < roman.size(); j++) {
+            for (int i = 0; i < roman.size(); i++) {
+                if (spl[0].equals(roman.get(i))) {
+                    spl[0] = arabic.get(i);
                 }
-                if (calculation % 10 != 0) {
-                    int ss = calculation % 10;
-                    System.out.println(roman.get(ss - 1));
 
+                if (spl[2].equals(roman.get(j))) {
+                    spl[2] = arabic.get(j);
+
+
+                    int spl0 = Integer.parseInt(spl[0]);
+                    int spl2 = Integer.parseInt(spl[2]);
+                    String math = spl[1];
+                    int calculation = Result(spl0, spl2, math);
+
+
+                    if (calculation == 100) {
+                        System.out.println("С");
+                    } else if (calculation % 10 == 0) {
+                        int s = calculation / 10;
+                        System.out.print(romanTens.get(s - 1));
+                    } else if (((calculation % 100) / 10) != 0) {
+                        int s = (calculation % 100) / 10;
+                        System.out.print(romanTens.get(s - 1));
+                    }
+                    if (calculation % 10 != 0) {
+                        int ss = calculation % 10;
+                        System.out.println(roman.get(ss - 1));
+
+                    }
                 }
             }
         }
